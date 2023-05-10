eb427b9ea82b0a0781ec152b7b3e46a09005a9be
AnythingRed <130374881+AnythingRed@users.noreply.github.com>
2023-05-10 15:20:59 +0200

    Add background changes based on time

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+let h;
+let m;
+let s;
+let ampm;
+
 const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
 const ampmEl = document.getElementById('ampm');
 
-function getTime() {
-   let h = new Date().getHours();
-   let m = new Date().getMinutes();
-   let s = new Date().getSeconds();
-   let ampm = 'AM'
+function updateClock() {
+   h = new Date().getHours();
+   m = new Date().getMinutes();
+   s = new Date().getSeconds();
+   ampm = 'AM'
 
    if (h > 12) {
       h = h - 12;
       ampm ='PM';
    }
+   
+   changeBG();
 
    h = h < 10 ? '0' + h: h;
    m = m < 10 ? '0' + m: m;
@@ -23,8 +30,26 @@ function getTime() {
    secondsEl.innerText = s;
    ampmEl.innerText = ampm;
    setTimeout(() => {
-      getTime();
+      updateClock();
    }, 1000);
 }
 
-getTime()
+function changeBG() {
+   if (h >= 5 && h <= 8 && ampm === 'AM') {
+      document.body.style.background = "url('./img/sunrise.jpg')";
+   } else if (h >= 9 && h <= 11 && ampm === 'AM' ||
+      h <= 5 && ampm === 'PM' ||
+      h === 12 && ampm === 'PM') {
+      document.body.style.background = "url('./img/day.jpg')";
+   } else if (h >= 6 && h <= 8 && ampm === 'PM') {
+      document.body.style.background = "url('./img/sunset.jpg')";
+   } else if (h >= 9 && h <= 11 && ampm === 'PM' ||
+      h >= 1 && h <= 4 && ampm === 'AM' ||
+      h === 12 && ampm === 'AM') {
+      document.body.style.background = "url('./img/night.jpg')";
+      document.getElementsByTagName('h1')[0].style.color = 'lightskyblue'
+   }
+   document.body.style.backgroundSize = 'cover';
+}
+
+updateClock()
